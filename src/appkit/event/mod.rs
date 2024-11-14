335b34cdc8f14f37bd5e2767aaa5e9e7884c1406
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-14 21:53:16 +0700

    Add numpad and help keys support in modifer flags key event info

--- src/appkit/event/mod.rs
+++ src/appkit/event/mod.rs
@@ -209,6 +209,8 @@ pub enum EventModifierFlag {
     Control,
     Option,
     Command,
+    Numpad,
+    Help,
     Function,
     DeviceIndependentFlagsMask
 }
@@ -221,6 +223,8 @@ impl From<EventModifierFlag> for NSUInteger {
             EventModifierFlag::Control => 1 << 18,
             EventModifierFlag::Option => 1 << 19,
             EventModifierFlag::Command => 1 << 20,
+            EventModifierFlag::Numpad => 1 << 21,
+            EventModifierFlag::Help => 1 << 22,
             EventModifierFlag::Function => 1 << 23,
             EventModifierFlag::DeviceIndependentFlagsMask => 0xffff0000
         }
@@ -235,6 +239,8 @@ impl From<&EventModifierFlag> for NSUInteger {
             EventModifierFlag::Control => 1 << 18,
             EventModifierFlag::Option => 1 << 19,
             EventModifierFlag::Command => 1 << 20,
+            EventModifierFlag::Numpad => 1 << 21,
+            EventModifierFlag::Help => 1 << 22,
             EventModifierFlag::Function => 1 << 23,
             EventModifierFlag::DeviceIndependentFlagsMask => 0xffff0000
         }
